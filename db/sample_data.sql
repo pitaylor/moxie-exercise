407c9d6ef0d5c3be24f285d9eb72709a332f10b4
Pete Taylor <peter.taylor@gmail.com>
2025-07-28 10:56:59 -0500

    Add sample data

--- db/sample_data.sql
+++ db/sample_data.sql
@@ -1,2 +1,3 @@
 INSERT INTO medspas (name, address, phone_number, email_address) VALUES 
-('Glow MedSpa', '123 Wellness Blvd, Beverly Hills, CA 90210', '(310) 555-0123', 'info@glowmedspa.com');
+('Glow MedSpa', '123 Wellness Blvd, Beverly Hills, CA 90210', '(310) 555-0123', 'info@glowmedspa.com')
+ON CONFLICT (name) DO NOTHING;
